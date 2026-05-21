bfcff01403842b6dd767d469d162895d0944aca5
Leehan Anoche <csanoche@gmail.com>
2026-05-21 21:45:29 +0800

    feat(backend.tf): add backend tf informatoin

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "aws-k3s-ha-platform-tf-bucket-720459752427-ap-southeast-1-an"
+    key            = "terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "aws-k3s-ha-platform-tf-lock"
+    encrypt        = true
+  }
+}
