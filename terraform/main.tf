787c96ea33ba0013c3d1a746442df83400dd2e40
Leehan Anoche <csanoche@gmail.com>
2026-05-24 12:33:23 +0800

    feat(main.tf): add aws setting in terraform

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+# Configure Terraform to use the AWS provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
