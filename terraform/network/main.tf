8c7ce08ad530e23797255f4ee0b6f5d450abd65e
Leehan Anoche <csanoche@gmail.com>
2026-05-25 21:39:22 +0800

    feat(network): add public subnet

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -13,3 +13,17 @@ resource "aws_vpc" "main" {
     }
   )
 }
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.16.16.0/28"
+  availability_zone = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+  
+  tags = merge(
+    local.common_tags,
+    {
+      Role = "network"
+    }
+  )
+}
