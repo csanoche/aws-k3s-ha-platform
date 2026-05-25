864e0f216c50e50281a9cd9f27cbc4470a1e1892
Leehan Anoche <csanoche@gmail.com>
2026-05-25 21:49:23 +0800

    feat(network): add private subnet

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -1,8 +1,9 @@
-# Create a VPC with a CIDR block of 10.16.16.0/24
+
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
+# Create a VPC with a CIDR block of 10.16.16.0/24
 resource "aws_vpc" "main" {
   cidr_block = "10.16.16.0/24"
 
@@ -14,12 +15,27 @@ resource "aws_vpc" "main" {
   )
 }
 
+# Create public subnet with public IP mapping enabled
 resource "aws_subnet" "public" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.16.16.0/28"
   availability_zone = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
   
+  tags = merge(
+    local.common_tags,
+    {
+      Role = "network"
+    }
+  )
+}
+
+# Create private subnet without public IP mapping
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.16.16.16/28"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
   tags = merge(
     local.common_tags,
     {
