78d5fe062724beff8df2f3da720c5fb3b97679df
Leehan Anoche <csanoche@gmail.com>
2026-05-27 19:48:26 +0800

    feat(network): add route table and assoc from private subnet to nat gateway

--- terraform/network/network.tf
+++ terraform/network/network.tf
@@ -108,3 +108,18 @@ resource "aws_subnet" "private" {
     }
   )
 }
+
+resource "aws_route_table" "nat_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main_nat.id
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.nat_rt.id
+}
+
