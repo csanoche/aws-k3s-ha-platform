451c0ad3dc8c6b30bdeca824db292dddd7b93af3
Leehan Anoche <csanoche@gmail.com>
2026-05-27 20:04:38 +0800

    feat(network): set-up security group for bastion

--- terraform/network/network.tf
+++ terraform/network/network.tf
@@ -123,3 +123,32 @@ resource "aws_route_table_association" "private_assoc" {
   route_table_id = aws_route_table.nat_rt.id
 }
 
+### Security Groups ###
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion_sg"
+  description = "Allow SSH access and TLS traffic to the bastion host, allow all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    {
+      Role = "network"
+    }
+  )
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.bastion_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.bastion_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # all ports
+}
+
