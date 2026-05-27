e8777c1d2996c105c8cfe83c611d3a9c537a63c4
Leehan Anoche <csanoche@gmail.com>
2026-05-27 20:30:45 +0800

    feat(network): add common_sg for private subnet instances

--- terraform/network/network.tf
+++ terraform/network/network.tf
@@ -152,3 +152,29 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   ip_protocol       = "-1" # all ports
 }
 
+resource "aws_security_group" "common_sg" {
+  name        = "common_sg"
+  description = "Allow SSH from bastion host, allow all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    {
+      Role = "network"
+    }
+  )
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_from_bastion_ipv4" {
+  security_group_id = aws_security_group.common_sg.id
+  referenced_security_group_id = aws_security_group.bastion_sg.id
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.common_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # all ports
+}
