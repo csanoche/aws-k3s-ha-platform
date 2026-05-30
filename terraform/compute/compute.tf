a1b112cf66c98468fc1a3bc2f3a14931b8a0d5bc
Leehan Anoche <csanoche@gmail.com>
2026-05-30 21:09:05 +0800

    feat(compute): add bastion host ec2 config

---
+++ terraform/compute/compute.tf
@@ -0,0 +1,37 @@
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+
+# Create EC2 instance for bastion host (1)
+resource "aws_instance" "bastion_host" {
+  ami           = data.aws_ami.amzn-linux-2023-ami.id
+  instance_type = "t3.nano"
+
+  subnet_id = aws_subnet.public.id
+  vpc_security_group_ids = [ aws_security_group.bastion_sg.id ]
+  key_name = aws_key_pair.ansible_ssh_key.key_name
+
+
+  tags = merge(
+    var.common_tags,
+    {
+      Role = "bastion"
+    }
+  )
+}
+
+# Create EC2 instances for k3s master (3)
+
+# Create EC2 instances for k3s worker nodes (3)
+
+# Create EC2 instance for HAproxy load balancers (2)
+
+# Create EC2 instance for postgres database (3)
+
